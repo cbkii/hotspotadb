298d42ddf1317232d0e23ff895c25a292477edf2
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-12 15:07:20 +1000

    refactor: isolate ADB manager binder access

---
+++ app/src/main/kotlin/io/drsr/hotspotadb/compat/AdbManagerCompat.kt
@@ -0,0 +1,25 @@
+package io.drsr.hotspotadb.compat
+
+import android.os.IBinder
+
+/** Compatibility access to adbd's current wireless TLS port. */
+object AdbManagerCompat {
+    fun getWirelessPort(): Int =
+        try {
+            val serviceManager = Class.forName("android.os.ServiceManager")
+            val binder =
+                serviceManager
+                    .getMethod("getService", String::class.java)
+                    .invoke(null, "adb") as? IBinder ?: return -1
+            val stub = Class.forName("android.debug.IAdbManager\$Stub")
+            val service =
+                stub
+                    .getMethod("asInterface", IBinder::class.java)
+                    .invoke(null, binder) ?: return -1
+            service.javaClass.getMethod("getAdbWirelessPort").invoke(service) as? Int ?: -1
+        } catch (_: ReflectiveOperationException) {
+            -1
+        } catch (_: SecurityException) {
+            -1
+        }
+}
